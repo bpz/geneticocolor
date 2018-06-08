4ad1d62ac4499ab0d4ffc5ff67639411347e7c2e
bpz <nammy.he@gmail.com>
2018-06-08 10:36:02 +0200

    Adding module comments
    Adding comments for __init__.py

--- geneticocolor/__init__.py
+++ geneticocolor/__init__.py
@@ -1,2 +1,22 @@
 # -*- coding: utf-8 -*-
+
+"""
+Geneticocolor
+========
+Geneticocolor is a Python package for color sets generation in 2D plots, based in oRGB color space.
+Source::
+    https://github.com/bpz/geneticocolor
+Simple example
+--------------
+Generate colors for a set of points and plot them using matplotlib:
+    >>> import geneticocolor.color_generator as generator
+    >>> import matplotlib.pyplot as plt
+    ...
+    >>> colors = generator.generate(x, y, point_classes)
+    >>> plt.scatter(x, y, colors)
+License
+-------
+GNU General Public License v3.0
+"""
+
 name = "geneticocolor"
