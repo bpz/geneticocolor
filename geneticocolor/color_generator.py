07e1839fc2219974d7a125f7843060fdef7aa7b2
bpz <beatriz.hpz@gmail.com>
2018-06-16 19:18:43 +0200

    documentación para color_generator

--- geneticocolor/color_generator.py
+++ geneticocolor/color_generator.py
@@ -1,3 +1,10 @@
+"""
+color_generator
+===============
+
+Generate a set of colors for N points with K classes, using oRGB space color.
+"""
+
 from deap import creator, base, tools, algorithms
 from collections import OrderedDict, Counter
 from geneticocolor.oRGB import RGB_to_oRGB, oRGB_to_RGB, randcolor_RGB, randcolor_oRGB
@@ -5,9 +12,6 @@ from random import random, randint
 from time import time
 import numpy
 
-"""
-Esto es un comentario de prueba
-"""
 
 def _centroid(pointArray):
     length = pointArray.shape[0]
@@ -17,22 +21,13 @@ def _centroid(pointArray):
 def _distances_centroid(N, K, points, points_classes):
     geom_distances = numpy.zeros([K,K])
     centroids = numpy.zeros([K,2])
-    std_d = [0] * K
     for i in range (0, K):
         points_i = [points[index] for index in range(0,N) if points_classes[index] == i]
         centroids[i] = _centroid(numpy.array(points_i))
-        std_d[i] = numpy.std(numpy.array(points_i))
-    print (std_d)
     for i in range (0, K):
         j = i + 1
         while (j < K):
             d = numpy.linalg.norm(centroids[i] - centroids[j])
-            print ("d original", d)
-            e = d -(std_d[i] + std_d[j])
-            if e < 0:
-                e = 0
-            print ("d cambiada", e)
-            print (d, "\n")
             geom_distances[i][j] = d
             geom_distances[j][i] = d
             j += 1
@@ -48,24 +43,6 @@ def _distances(N, K, points, points_classes):
                 geom_distances[j][i] = geom_distances[i][j]
             j += 1
     return geom_distances
-
-def _objective(geom_distances, points_classes, counter_classes, indiv):
-    K = len(indiv)
-    N = geom_distances.shape[0]
-    color_distances = numpy.zeros([K, K])
-    res = 0 # resultado
-
-    for i in range(0,N): # para todos los points
-        ki = points_classes[i]
-        j = i + 1
-        while(j < N):
-            kj = points_classes[j]
-            if geom_distances[i][j] != 0:
-                if (color_distances[ki][kj] == 0):
-                    color_distances[ki][kj] = numpy.linalg.norm(numpy.array(indiv[ki]) - numpy.array(indiv[kj]))
-                res +=  color_distances[ki][kj] / geom_distances[i][j]
-            j += 1
-    return res,
 
 def _objective_norm(geom_distances, points_classes, counter_classes, indiv):
     K = len(indiv)
@@ -133,8 +110,6 @@ def _values_params(mode, NGEN, TAM_POPULATION, mating_prob,  mating_mutation_pro
         _NGEN = 300
         _use_time = False
 
-    print(_NGEN, _TAM_POPULATION, _mating_prob, _mating_mutation_prob, _mutation_prob, _tournsize, _use_time)       
-
     return (_NGEN, _TAM_POPULATION, _mating_prob, _mating_mutation_prob, _mutation_prob, _tournsize, _use_time)
 
 def generate(x, y, points_classes,
@@ -149,6 +124,64 @@ def generate(x, y, points_classes,
     mutation_prob = 0.1,
     tournsize = 3):
     
+    """ Convert oRGB color into sRGB color.
+
+    Parameterss
+    -----------
+    x : float list
+        list of x components.
+    y : float list
+        list of y components.
+    mode : string
+        Avaible values: 'fast', 'custom' and 'optimized'.
+        Default value is 'fast'.
+    verbose: bool
+        Flag for displaying log messages. Default value is False.    
+    return_fitness_solution: bool
+        If true, the function will also return the fitness value of the solution.
+        Default value is False.
+    return_evolution_data: bool
+        Default value is False.
+        If true, the function will also return a list with best fitness value for every generation.
+        Default value is False.
+    NGEN: int
+        Number of generations in the genetic algorithm.
+        Default value for 'fast' mode is 20 and for 'optimized' is 300.
+    TAM_POPULATION: int.
+        Size of the genetic algorithm population.
+        Default value for 'fast' and 'optimized' modes is 10.
+    mating_prob: float
+        Probability of mating in crossover function. Value between 1 and 0. 
+        Default value for 'fast' and 'optimized' modes is 0.5
+    mating_mutation_prob: float
+        Probability of mutation during crossover function. Value between 1 and 0.
+        Default value for 'fast' and 'optimized' modes is 0.7
+    mutation_prob: float
+        Probability of mutation in mutation function. Value between 1 and 0.
+        Default value for 'fast' and 'optimized' modes is 0.1
+    tournsize: int
+        Size of the tournament in selection function.
+        Default value for 'fast' and 'optimized' modes is 3
+
+    Returns
+    -------
+    list : [c0, c1, c2, ..., ck-1]
+        List of size K (the number of point classes) with the generated colors in RGB space.
+        Each color is a three element list with values inside interval [0-1]
+
+    Notes
+    -----
+    There are three modes for using this function:
+    -   'fast': option by default. Uses default values for algorithm params.
+    -   'optimized': same as 'fast', but with more generations. This option is
+        indicated for cases with K >= 20. Notice that it will increase executing time.
+    -   'custom': it will take params indicated in the call of the function.
+
+    Fast mode is indicated for 20 diferent classes or below. It will provide best solution generated in 2.5s or at least 20 generations.
+    In case there is a need for using diferent params, custom mode allows customization of all parameters.
+    If only more executions are needed, optimized mode uses 300 generations, but that will also increase notably execution time.
+    """
+
     classes = list(OrderedDict.fromkeys(points_classes))
     points_classes = numpy.array([classes.index(i) for i in points_classes])
     points = numpy.array(list(zip(x,y)))
